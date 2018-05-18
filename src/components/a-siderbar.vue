d2b130de3f88cf20027e70f2c6b6d23e73040be4
Jmhh <fscs10@hotmail.com>
2018-05-18 11:01:36 +0800

    added 框架

---
+++ src/components/a-siderbar.vue
@@ -0,0 +1,82 @@
+<!-- 管理端首页侧边栏 -->
+<template>
+    <div class="admin-siderbar">
+        <ul>
+            <li>
+                <a href="#"><img src="../images/logo.png" alt=""></a>
+            </li>
+            <li v-for="item in items" :key="item.name">
+                <router-link :to="item.href">
+                    <i :class="item.icon"></i>
+                    <p>{{item.name}}</p>
+                </router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    href: "index",
+                    icon: "iconfont icon-shouye",
+                    name: "首页"
+                },
+                {
+                    href: "school",
+                    icon: "iconfont icon-tubiaozhizuomoban-",
+                    name: "学校"
+                },
+                {
+                    href: "guanli",
+                    icon: "iconfont icon-guanliyuan",
+                    name: "管理"
+                }
+            ]
+        };
+    },
+    mounted() {
+        let Rname = this.$route.name;
+    }
+};
+</script>
+<style rel='stylesheet/scss' lang='scss' scoped>
+.admin-siderbar {
+    width: 80px;
+    height: 100%;
+    min-height: 800px;
+    background: #4b83f0;
+    border-radius: 20px 0 0 20px;
+    ul {
+        list-style: none;
+        margin: 0;
+        padding-left: 0;
+        li {
+            a {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                width: 100%;
+                height: 80px;
+                color: #fff;
+                text-decoration: none;
+                border-bottom: 1px solid #73a3ff;
+                i {
+                    font-size: 32px;
+                }
+            }
+            a.router-link-exact-active.active {
+                background: #62d862;
+            }
+            p {
+                font-size: 12px;
+                padding-top: 5px;
+            }
+        }
+    }
+}
+</style>
---
+++ src/pages/user/tabs/tabs.vue
@@ -0,0 +1,127 @@
+<!-- tabs组件 -->
+<template>
+
+    <div class="box">
+        <ul class="tabs clearfix">
+            <li v-for="(tab,index) in tabsName">
+
+                <a href="javascript:;" class="tab-link" @click="tabsSwitch(index)" v-bind:class="{active:tab.isActive}">
+                    <i :class="tab.icon"></i> {{tab.name}}</a>
+            </li>
+        </ul>
+
+        <div class="cards">
+            <div class="tab-card" style="display: block;">班级</div>
+            <div class="tab-card" style="display: none;">老师</div>
+            <div class="tab-card" style="display: none;">学生</div>
+            <div class="tab-card" style="display: none;">课本管理</div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tabsName: [
+                {
+                    icon: "iconfont icon-ai-connection",
+                    name: "班级",
+                    isActive: true
+                },
+                {
+                    icon: "iconfont icon-ai-connection",
+                    name: "老师",
+                    isActive: false
+                },
+                {
+                    icon: "iconfont icon-ai-connection",
+                    name: "学生",
+                    isActive: false
+                },
+                {
+                    icon: "iconfont icon-ai-connection",
+                    name: "课本管理",
+                    isActive: false
+                }
+            ],
+            active: false
+        };
+    },
+    methods: {
+        tabsSwitch: function(tabIndex) {
+            var tabCardCollection = document.querySelectorAll(".tab-card"),
+                len = tabCardCollection.length;
+
+            for (var i = 0; i < len; i++) {
+                tabCardCollection[i].style.display = "none";
+                this.tabsName[i].isActive = false;
+            }
+            this.tabsName[tabIndex].isActive = true;
+            tabCardCollection[tabIndex].style.display = "block";
+        }
+    }
+};
+</script>
+<style rel='stylesheet/scss' lang='scss' scoped>
+.box {
+    height: 200px;
+    margin: 36px 24px;
+    .tabs > li {
+        width: 134px;
+        float: left;
+        list-style: none;
+    }
+    .tabs .tab-link {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-left: -1px;
+        line-height: 47px;
+        font-size: 16px;
+        font-family: PingFang-SC-Heavy;
+        color: rgba(163, 173, 189, 1);
+        text-decoration: none;
+        background: #f8f9fa;
+        border: 2px solid #edf0f5;
+        border-bottom: none;
+        border-radius: 5px 5px 0 0;
+        > i{
+           margin-right: 10px;
+        }
+    }
+
+    .tabs .tab-link.active {
+        color: rgba(58, 71, 96, 1);
+        height: 47px;
+        -webkit-transition: 0.3s;
+        transition: 0.3s;
+        background: #fff;
+        > i{
+            font-size: 30px;
+            color: #4b83f0;
+        }
+    }
+}
+
+.cards {
+    width: 100%;
+    float: left;
+    background: #fff;
+    border: 2px solid #edf0f5;
+    border-radius: 0 8px 8px 8px;
+    .cards .tab-card {
+        display: none;
+    }
+    .clearfix:after {
+        content: "";
+        display: block;
+        height: 0;
+        clear: both;
+    }
+    .clearfix {
+        zoom: 1;
+    }
+}
+</style>
